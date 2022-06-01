90711609afd74699e18d090efd79df2c2fef5a12
kerryon <56770828+kerryon@users.noreply.github.com>
2022-06-01 09:50:51 +0200

    initial

---
+++ js/quotes.js
@@ -0,0 +1,141 @@
+var checkIn = [
+	{
+		quote: "Wofür bist du diese Woche/heute dankbar?",
+		tags: ['Freunde']
+	},
+	{
+		quote: "Wann hast du das letzte Mal etwas zum ersten Mal gemacht?",
+		tags: ['Freunde']
+	},
+	{
+		quote: "Was hat dich zuletzt so inspiriert/begeistern, dass du anderen davon erzählt hast?",
+		tags: ['Freunde']
+	},
+	{
+		quote: "Welchem aktuellen Trend sollten wir als Team/Unternehmen mehr folgen?",
+		tags: ['Freunde']
+	},
+	{
+		quote: "Was ist dein meist gehörter Interpret auf Spotify/iTunes...? ",
+		tags: ['Freunde']
+	},
+	{
+		quote: "Welches Lied drückt deine Stimmung heute aus?",
+		tags: ['Freunde']
+	},
+	{
+		quote: "Wessen Job würdest du gerne mal machen?",
+		tags: ['Freunde']
+	},
+	{
+		quote: "Welchen Berufswunsch hattest du als Kind?",
+		tags: ['man kennt sich']
+	},
+	{
+		quote: "Was ist für dich der schönste Ort, an dem du jemals warst?",
+		tags: ['man kennt sich']
+	},
+	{
+		quote: "Auf welches Lebensmittel könntest du getrost verzichten?",
+		tags: ['man kennt sich']
+	},
+	{
+		quote: "Was sind die Top-3 News aus deinem Bereich/Team/Unternehmen?",
+		tags: ['man kennt sich']
+	},
+	{
+		quote: "Was könnte deine Aufmerksamkeit heute ablenken?",
+		tags: ['man kennt sich']
+	},
+	{
+		quote: "Unter welchem Hashtag läuft dein Tag / deine Woche?",
+		tags: ['man kennt sich']
+	},
+	{
+		quote: "Stadtmensch oder Landei?",
+		tags: ['man kennt sich']
+	},
+	{
+		quote: "Was ist für dich das Unwort des Jahres?",
+		tags: ['Fremde']
+	},
+	{
+		quote: "Morgenmensch oder Nachteule? ",
+		tags: ['Fremde']
+	},
+	{
+		quote: "Welches Buch hast du zuletzt gelesen / welchen Podcast zuletzt gehört?",
+		tags: ['Fremde']
+	},
+	{
+		quote: "Wenn du ein bedingungsloses Grundeinkommen bekommen würdest, welchen Job würdest du dann machen?",
+		tags: ['Fremde']
+	},
+	{
+		quote: "Welche Sprache würdest du gerne sprechen können?",
+		tags: ['Fremde']
+	},
+	{
+		quote: "Was sind deine 3 Wünsche für den Workshop / das Meeting heute?",
+		tags: ['Fremde']
+	},
+	{
+		quote: "Mit was können dir andere immer eine Freude machen?",
+		tags: ['Fremde']
+	}
+];
+
+var checkOut = [
+	{
+		quote: "Was war dein AHA-Moment in diesem Meeting/Workshop/heute?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Wie fandest du die Team-Zusammenarbeit auf einer Skala von 1-10? Warum?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Nenne 1 Sache, die wir beim nächsten Meeting/Workshop beibehalten und 1 Sache, die wir besser machen sollten?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Mit wem aus dem Workshop/Meeting würdest du gerne nochmal 10min in einen Austausch gehen?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Wie sollte die Team-Konstellation beim nächsten Mal aussehen? Fehlt jemand, sind wir zu viele, welche Perspektive braucht es noch, fehlen Rollen?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Gib jemanden aus dem Team ein Lob / Dank.",
+		tags: ['check-out']
+	},
+	{
+		quote: "Welches Thema hat dich heute besonders vorangebracht? / Welches garnicht?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Was war dein Highlight & Lowlight?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Wie schätzt du deinen eigenen Beitrag bei dem Meeting / Workshop heute ein? Was nimmst du dir vor beim nächsten Mal besser/anders zu machen?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Wenn unser Meeting/Workshop /Projekt ein Film gewesen ist, welcher war es? Bzw. Mit welchem Filmgenre ist unser Meeting / Workshop / Projekt heute vergleichbar? (z.B. Drama, Thriller, Komödie, Liebesfilm, Kinderfilm, Tier-Dokumentation, Science Fiction...)",
+		tags: ['check-out']
+	},
+	{
+		quote: "Welche Stärke von dir konntest du garnicht einbringen bisher?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Sind wir methodisch gut aufgestellt? Was lief gut, was weniger?",
+		tags: ['check-out']
+	},
+	{
+		quote: "Von wem könnten wir lernen wie wir es besser machen können? Welches Know-How benötigen wir?",
+		tags: ['check-out']
+	}
+];
---
+++ js/script.js
@@ -0,0 +1,90 @@
+// event listener to respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
+
+//variables declarations
+var randomObjects;
+var message = ''; //<==empty string
+var quotes;
+var red;
+var green;
+var blue;
+var quotes2 = []; //<==empty array
+var checkInChecked = document.getElementById("in");
+var checkOutChecked = document.getElementById("out");
+
+//a function to print the message in an attachable HTML element.
+function print(message) {
+  var outputDiv = document.getElementById("quote-box");
+  outputDiv.innerHTML = message;
+}
+
+//a function which selects a random number through the 255 rgb + 1 colors.
+function randomHEX() {
+  var bgcolors = ['#FCE847', '#4EFFA7', '#F796E1', '#FF8810', '#18DD7B'];
+  return bgcolors[Math.floor(Math.random()*bgcolors.length)];
+}
+
+//Function to genereate random rgb color value
+function randomColor() {
+  var color;
+  color = randomHEX();
+  return color;
+}
+
+//uncheck checked checkbox
+function uncheckChecked(obj) {
+  var cbs = document.getElementsByClassName("btn");
+  for (var i = 0; i < cbs.length; i++) {
+      cbs[i].checked = false;
+  }
+  obj.checked = true;
+}
+
+
+//Function named getRandomQuote which selects a random object from quotes array and returns the randomly selected object, then we splice it, to don't repeat a random quote more than once until all quotes from the array have been displayed.
+function getRandomQuote() {
+
+  if (checkInChecked.checked == true) {
+  var quoteObject = Math.floor(Math.random() * checkIn.length);
+  var splicedQuote = checkIn.splice(quoteObject, 1)[0];
+  quotes2.push(splicedQuote);
+	if (checkIn.length === 0) {
+		checkIn = quotes2;
+	}
+}
+if (checkOutChecked.checked == true) {
+  var quoteObject = Math.floor(Math.random() * checkOut.length);
+  var splicedQuote = checkOut.splice(quoteObject, 1)[0];
+  quotes2.push(splicedQuote);
+	if (checkOut.length === 0) {
+		checkOut = quotes2;
+	}
+}
+  return splicedQuote;
+}
+
+// Function to build a string using the different properties of the quote object previously selected from the array and print it to the screen. 
+function printQuote() {
+  changeEmoji();
+  randomObjects = getRandomQuote();
+  message = '<p class="quote">' + randomObjects.quote + '</p>'; 
+  if (randomObjects.source !== undefined) {
+    message += '<p class="source">' + randomObjects.source;
+  }
+  if (randomObjects.citation !== undefined && randomObjects.year !== undefined) {
+    message += '<span class="citation">' + randomObjects.citation + '</span>'; 
+    message += '<span class="year">' + randomObjects.year + '</span>' + '</p>';
+  }
+  message += '<h4>' + randomObjects.tags + '</h4>';
+  print(message);
+  randomColor();
+  document.getElementById('rgb').style.background = randomHEX();
+}
+//Set an interval of each 10 seconds to execute our printQuote function
+// setInterval(printQuote, 10000);
+
+function changeEmoji() {
+  const root = document.querySelector(":root");
+  root.style.setProperty("--pseudo-emoji", `'👉🏽'`);
+}
